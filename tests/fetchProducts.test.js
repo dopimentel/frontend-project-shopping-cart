8cc7d682ac506204bc7e62a4ef8729aaf6b629c3
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-14 16:55:15 -0300

    Requisito 1 - teste 5

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -19,5 +19,8 @@ describe('1 - Teste a função fetchProducts', () => {
     const result = await fetchProducts("computador");
     expect(result).toEqual(computadorSearch);
   });
-
+  it('', async () => {
+    const failedRequest = await fetchProducts();
+    expect(failedRequest).toEqual(new Error('You must provide an url'));
+  });
 });
