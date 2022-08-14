a810ed901e27215be8e8b1eb4c4850b7c1768c48
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-14 16:38:51 -0300

    Requisito 1 - teste 3

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,8 +6,14 @@ describe('1 - Teste a função fetchProducts', () => {
   it ('is a function', () => {
     expect(typeof fetchProducts).toMatch('function');
   })
-  it ('has been called with "computador"', async () => {
+  it ('fetch has been called when "computador" is their parameter', async () => {
     await fetchProducts('computador');
     expect(fetch).toHaveBeenCalled();
   })
+  it('fetch has been called with endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador" when their parameter is "computador"', async () => {
+    await fetchProducts("computador");
+    const URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toHaveBeenCalledWith(URL);
+  });
+  
 });
