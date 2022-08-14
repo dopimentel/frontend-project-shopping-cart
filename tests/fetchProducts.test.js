b055ed5dc34d9209b29d6de1249720ebc0ae1305
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-14 16:21:43 -0300

    Requisito 1 - teste 2

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,4 +6,8 @@ describe('1 - Teste a função fetchProducts', () => {
   it ('is a function', () => {
     expect(typeof fetchProducts).toMatch('function');
   })
+  it ('has been called with "computador"', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  })
 });
