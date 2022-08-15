389544e885bcd27cab15481d4bb1513fd79977c9
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-15 11:04:30 -0300

    Requisito 6 - teste 2

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -8,4 +8,8 @@ describe('3 - Teste a função saveCartItems', () => {
     saveCartItems('<ol><li>Item</li></ol>')
     expect(localStorage.setItem).toHaveBeenCalled();
   })
+  it('', () => {
+    saveCartItems('<ol><li>Item</li></ol>')
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  })
 });
