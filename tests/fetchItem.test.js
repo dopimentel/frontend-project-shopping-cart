4287cc0450a0a8c9233884fb0c2a705a75969207
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-14 22:19:22 -0300

    Requisito 3 - teste 2

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -4,6 +4,10 @@ const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
   it('is a function', () => {
-    expect(typeof fetchItem).toMatch("function");
+    expect(typeof fetchItem).toMatch('function');
+  })
+  it('', async () => {
+    await fetchItem('MLB1615760527')
+    expect(fetch).toHaveBeenCalled();
   })
 });
